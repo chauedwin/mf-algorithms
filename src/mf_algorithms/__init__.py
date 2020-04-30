66b421fedcc562daf116e756f6b62e6ec6eaa809
Edwin Chau <edwinchau@icloud.com>
2020-04-30 16:38:04 -0700

    package

---
+++ src/mf_algorithms/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for MF Algorithms."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.1"
+
+__author__ = "Edwin Chau"
+__email__ = "edwinchau@ucla.edu"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Edwin Chau"
+
+
+from .example import Example  # noqa: F401
