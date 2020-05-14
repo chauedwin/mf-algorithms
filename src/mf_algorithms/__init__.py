2e959a59ad161d1ba038a8570dd0be849f1a1c1e
Edwin Chau <edwinchau@icloud.com>
2020-05-13 18:12:42 -0700

    Bump version: 0.0.1 → 0.0.2

--- src/mf_algorithms/__init__.py
+++ src/mf_algorithms/__init__.py
@@ -2,7 +2,7 @@
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __author__ = "Edwin Chau"
 __email__ = "edwinchau@ucla.edu"
